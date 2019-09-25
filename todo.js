49f71610c6c8e0989e4f0be32cde80ab154f1e3d
tharunGr <tharun1997.tg@gmail.com>
2019-09-25 21:49:53 +0530

    deletion, checkbox

--- todo.js
+++ todo.js
@@ -1,10 +1,12 @@
 var millisec;
 var currentListId;
 var currentTaskId;
+var currentStepId;
 var totalList = [];
 var taskList = {id: "", name: "", task: [], status: ""};
-var task = {id: "", name: "", status: "", step: []};
-var step = {name: ""};
+var task = {id: "", name: "", status: "", isFinished: "", step: []};
+var step = {id: "", name: "", status: "", isFinished: ""};
+var taskEntries = 0;
 
 document.querySelector(".sideBarButton").addEventListener("click",function() {
 	//console.log(document.querySelector(".sideBarButton").value);
@@ -50,13 +52,25 @@ document.querySelector(".plusIcon").addEventListener("click", function(e) {
 	}
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
 var textBox = document.querySelector(".newListText");
 textBox.addEventListener('keyup',function(e) {
 	var listContent;
 	if ((textBox.value.length != 0) && (e.keyCode === 13)) {
 		listContent = textBox.value;
 		saveListInTotalList(listContent);
-		createNewList(listContent);
+		createNewList(listContent, "true");
 		textBox.value = null;
 	}
 });
@@ -77,7 +91,7 @@ function refreshList() {
 	})
 }
 
-function createNewList(listContent) {
+function createNewList(listContent, boolean) {
 	refreshList();
 	totalList.forEach(function(list) {
 		if (list.status == "active") {
@@ -92,19 +106,105 @@ function createNewList(listContent) {
 			var span = document.createElement('span');
 			span.textContent = list.name;
 			textDiv.appendChild(span);
+			var spanCount = document.createElement('span');
+			spanCount.setAttribute("class", "spanCount");
+			var count = 0;
+			if (typeof list.task !== 'undefined') {
+				list.task.forEach(function(task) {
+					if (task.isFinished == "false") {
+						count = count + 1;
+					}
+				});
+			}
+			spanCount.textContent = count;
+			iconDiv.appendChild(spanCount);
 			document.querySelector(".newCreateList").appendChild(iconDiv).appendChild(textDiv);
 		}
 	});
-	document.querySelector(".titleName").textContent = listContent;
-	currentListId = millisec;
-	refreshTasks();
+	if (boolean == "true") {
+		document.querySelector(".titleName").value = listContent;
+		currentListId = millisec;
+		refreshTasks();
+	}
 }
 
 document.querySelector(".deleteList").addEventListener("click",function() {
 	const list = totalList.find(list => list.id == currentListId);
 	let statusDelete = "inActive"
 	list.status = statusDelete;
-	createNewList("Task");
+	createNewList("Task", "true");
+});
+
+document.querySelector(".newCreateList").addEventListener("click",function(e) {
+	refreshTasks();
+	currentListId = e.target.id;
+	totalList.forEach(function(list) {
+		if (list.id == e.target.id) {
+		    document.querySelector(".titleName").value = list.name;
+		    displayListTasks(list);
+		}
+	});
+});
+
+document.querySelector(".newListText").addEventListener("focus", function(e) {
+	document.querySelector(".fa-plus").style.color = "#868077"; 
+	document.querySelector(".fa-plus").style.opacity = "0.7"; 
+});
+
+document.querySelector(".newListText").addEventListener("focusout", function(e) {
+	document.querySelector(".fa-plus").style.color = "#2078e5"; 
+	document.querySelector(".fa-plus").style.opacity = "1"; 
+});
+
+var listNameEdit = document.querySelector(".titleName");
+listNameEdit.addEventListener("keyup", function(e) {
+	if ((listNameEdit.value.length != 0) && (e.keyCode === 13)) {
+		const list = totalList.find(list => list.id == currentListId);
+		list.name = listNameEdit.value;
+		refreshList();
+		createNewList(list.name, "false");
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector(".task").addEventListener("click",function(e) {
+	currentTaskId = e.target.id;
+	const lists = totalList.find(list => list.id == currentListId);
+	const task = lists.task.find(task => task.id == currentTaskId);
+
+	checkBoxInStepHeader = document.querySelector("#checkboxInStepHeader");
+	taskInStepHeader = document.querySelector(".taskInStepHeader");
+	if (task.isFinished == "true") {
+		console.log("trueeeee" + task.name);
+		checkBoxInStepHeader.checked = true;
+		taskInStepHeader.setAttribute("value", task.name)
+		taskInStepHeader.setAttribute("class", "taskInStepHeader taskInStepHeaderStriked")
+	} else {
+		console.log("fasleeee" + task.name);
+		checkBoxInStepHeader.checked = false;
+		taskInStepHeader.setAttribute("value", task.name)
+		taskInStepHeader.setAttribute("class", "taskInStepHeader")
+	}
+	showStepsBar();
+	refreshSteps();
+	if (typeof task.step !== 'undefined') {
+		displaySteps(task.step);
+	}
 });
 
 document.querySelector(".addTask").addEventListener("click",function() {
@@ -124,25 +224,36 @@ document.querySelector(".addTask").addEventListener("click",function() {
 			const list = totalList.find(list => list.id == currentListId);
 			addTask(list);
 			document.querySelector(".newTaskText").value = null;
+			refreshList();
+			createNewList(list.name, "false");
 		}
 	});
 });
 
 function addTask(list) {
-	console.log("add task");
 	list.task.forEach(function(task) {
 		if (task.status == "active") {
 			var addedTask = document.createElement("DIV");
 			addedTask.setAttribute('class', 'addedTask');
 			addedTask.setAttribute('id', task.id);
-			var button = document.createElement("DIV");
-			button.setAttribute('class', 'plusAddedTaskIcon');
-			addedTask.appendChild(button);
-			var i = document.createElement("I");
-			i.setAttribute('class', 'far fa-circle');
-			button.appendChild(i);
+			var round = document.createElement("DIV");
+			round.setAttribute('class', 'round');
+			addedTask.appendChild(round);
+			var checkbox = document.createElement("input");
+			checkbox.type = "checkbox"; 
+			checkbox.id = task.id + "checkBox";
+			round.appendChild(checkbox);
+			listenerForCheckBoxInAddTask(checkbox);
+			var label = document.createElement('label'); 
+			label.htmlFor = task.id + "checkBox"; 
+			round.appendChild(label);
 			var span = document.createElement('span');
-			span.setAttribute('class', 'taskName');
+			if (task.isFinished == "true") {
+				span.setAttribute('class', "taskName strike");
+				checkbox.checked = true;
+		    } else {
+				span.setAttribute('class', 'taskName');
+			}
 			span.textContent = task.name;
 			addedTask.appendChild(span);
 			document.querySelector(".task").appendChild(addedTask);
@@ -150,24 +261,13 @@ function addTask(list) {
 	});
 }
 
-document.querySelector(".trashIcon").addEventListener("click",function() {
-	console.log("trash icon");
-	const list = totalList.find(list => list.id == currentListId);
-	const task = list.task.find(task => task.id == currentTaskId);
-	let statusDelete = "inActive"
-	task.status = statusDelete;
-	refreshTasks();
-	document.querySelector(".titleName").textContent = list.name;
-	displayListTasks(list);
-});
-
-
 function saveTaskInList(newTask) {
 	totalList.forEach(function(list) {
 		if (list.id == currentListId) {
 			let task = {};
 			task.name = newTask;
 			task.id = getDate();
+			task.isFinished = "false";
 			let statusActive = "active";
 			task.status = statusActive;
 			if (typeof list.task === 'undefined') {
@@ -180,25 +280,7 @@ function saveTaskInList(newTask) {
 	});
 }
 
-document.querySelector(".newCreateList").addEventListener("click",function(e) {
-	refreshTasks();
-	currentListId = e.target.id;
-	totalList.forEach(function(list) {
-		if (list.id == e.target.id) {
-		    document.querySelector(".titleName").textContent = list.name;
-		    displayListTasks(list);
-		}
-	});
-});
-
-function getDate() {
-	date = new Date();
-	millisec = date.getTime();
-	return millisec;
-}
-
 function refreshTasks() {
-	console.log("refresh task");
     var refreshTaskBar = document.querySelectorAll(".addedTask");
     refreshTaskBar.forEach(function(task) {
     	document.querySelector(".task").removeChild(task);
@@ -206,21 +288,68 @@ function refreshTasks() {
 }
 
 function displayListTasks(lists) {
-	console.log("display task");
 	addTask(lists);
 }
 
-document.querySelector(".newListText").addEventListener("focus", function(e) {
-	console.log("in");
-	document.querySelector(".fa-plus").style.color = "#868077"; 
-	document.querySelector(".fa-plus").style.opacity = "0.7"; 
+function listenerForCheckBoxInAddTask(element) {
+	element.addEventListener("change", function(event) {
+		var id = event.target.id;
+		var taskId = id.replace("checkBox","");
+		currentTaskId = taskId;
+		const list = totalList.find(list => list.id == currentListId);
+		list.task.forEach(function(task) {
+			if(task.id == currentTaskId) {
+				checkBoxInStepHeader = document.querySelector("#checkboxInStepHeader");
+				taskInStepHeader = document.querySelector(".taskInStepHeader");
+		        if (element.checked) {
+					task.isFinished = "true";
+					checkBoxInStepHeader.checked = true;
+					taskInStepHeader.setAttribute("value", task.name)
+					taskInStepHeader.setAttribute("class", "taskInStepHeader taskInStepHeaderStriked")
+				} else {
+					task.isFinished = "false";
+					checkBoxInStepHeader.checked = false;
+					taskInStepHeader.setAttribute("value", task.name)
+					taskInStepHeader.setAttribute("class", "taskInStepHeader")
+				}
+				refreshTasks();
+				addTask(list);
+				refreshList();
+				createNewList(list.name, "false");
+			}
+		});
+	});
+}
+
+var taskNameEdit = document.querySelector(".taskInStepHeader");
+taskNameEdit.addEventListener("keyup", function(e) {
+	if ((taskNameEdit.value.length != 0) && (e.keyCode === 13)) {
+		const list = totalList.find(list => list.id == currentListId);
+		list.task.forEach(function(task) {
+			if(task.id == currentTaskId) {
+				task.name = taskNameEdit.value;
+				taskNameEdit.value = task.name
+				refreshTasks();
+				displayListTasks(list);
+			}
+		});
+	}
 });
 
-document.querySelector(".newListText").addEventListener("focusout", function(e) {
-	console.log("in");
-	document.querySelector(".fa-plus").style.color = "#2078e5"; 
-	document.querySelector(".fa-plus").style.opacity = "1"; 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -237,29 +366,53 @@ document.querySelector(".addSteps").addEventListener("click",function() {
 	newStepText.addEventListener("keyup",function(e) {
 		if ((newStepText.value.length != 0) && (e.keyCode === 13)) {
 			var newStep = newStepText.value;
-			addStep(newStep);
 			saveStepInTask(newStep);
+			refreshSteps();
+			const list = totalList.find(list => list.id == currentListId);
+			const task = list.task.find(task => task.id == currentTaskId);
+			displaySteps(task.step);
 			document.querySelector(".newStepText").value = null;
 		}
 	});
 });
 
-function addStep(newStep) {
-	  var addedStep = document.createElement("DIV");
-	  addedStep.setAttribute('class', 'addedStep');
-	  addedStep.setAttribute('id', getDate());
-	  var button = document.createElement("BUTTON");
-	  button.setAttribute('class', 'plusAddedStepIcon');
-	  addedStep.appendChild(button);
-	  var i = document.createElement("I");
-	  i.setAttribute('class', 'far fa-circle');
-	  button.appendChild(i);
-	  var span = document.createElement('span');
-	  span.setAttribute('class', 'stepName');
-	  span.textContent = newStep;
-	  addedStep.appendChild(span);
-	  document.querySelector(".newSteps").appendChild(addedStep);
+function addStep(step) {
+	step.forEach(function(newStep) {
+		if (newStep.status == "active") {
+			var addedStep = document.createElement("DIV");
+			addedStep.setAttribute('class', 'addedStep');
+			addedStep.setAttribute('id', newStep.id);
+			var roundStepBarIcon = document.createElement("DIV");
+			roundStepBarIcon.setAttribute('class', 'roundStepBarIcon');
+			var checkboxStepBar = document.createElement("input");
+			checkboxStepBar.type = "checkbox"; 
+			checkboxStepBar.id = newStep.id + "checkBox";
+			roundStepBarIcon.appendChild(checkboxStepBar);
+			listenerForCheckBoxInAddStep(checkboxStepBar);
+			var labelStepBar = document.createElement('label'); 
+			labelStepBar.htmlFor = newStep.id + "checkBox"; 
+			roundStepBarIcon.appendChild(labelStepBar);
+			var spanBar = document.createElement('span');
+			if (newStep.isFinished == "true") {
+				spanBar.setAttribute('class', "stepTextBar stepTextBarStrike");
+				checkboxStepBar.checked = true;
+			} else {
+				spanBar.setAttribute('class', 'stepTextBar');
+				checkboxStepBar.checked = false;
+			}
+			spanBar.textContent = newStep.name;
+			var closeIconInAddStep = document.createElement("I");
+			closeIconInAddStep.setAttribute("class", "fa fa-times");
+			closeIconInAddStep.setAttribute("id", newStep.id + "close");
+			listenerForCloseIconInAddStep(closeIconInAddStep);
+			addedStep.appendChild(roundStepBarIcon);
+			addedStep.appendChild(spanBar);
+			addedStep.appendChild(closeIconInAddStep);
+			document.querySelector(".newSteps").appendChild(addedStep);
+		}
+	});
 }
+
 
 function saveStepInTask(newStep) {
 	totalList.forEach(function(list) {
@@ -267,7 +420,10 @@ function saveStepInTask(newStep) {
 			list.task.forEach(function(task) {
 				if(task.id == currentTaskId) {
 					let step = {};
+					step.id = getDate();
 					step.name = newStep;
+					step.status = "active"
+					step.isFinished = "false";
 					if (typeof task.step === 'undefined') {
 						task.step = [];
 						task.step.push(step);
@@ -280,24 +436,12 @@ function saveStepInTask(newStep) {
 	});
 }
 
-document.querySelector(".task").addEventListener("click",function(e) {
-	currentTaskId = e.target.id;
-	const lists = totalList.find(list => list.id == currentListId);
-	const task = lists.task.find(task => task.id == e.target.id);
-	document.querySelector(".stepText").value = task.name
-	showStepsBar();
-	refreshSteps();
-	displaySteps(task.step);
-});
-
 function displaySteps(step) {
-	step.forEach(function(step) {
-    	addStep(step.name);
-    });
+	addStep(step);
 }
 
 function refreshSteps() {
-    var refreshStepBar = document.querySelectorAll(".addedStep");
+	var refreshStepBar = document.querySelectorAll(".addedStep");
     refreshStepBar.forEach(function(step) {
     	document.querySelector(".newSteps").removeChild(step);
     });
@@ -317,6 +461,90 @@ function showStepsBar() {
 	}	
 }
 
+function listenerForCheckBoxInAddStep(element) {
+	element.addEventListener("change", function(event) {
+		var id = event.target.id;
+		var stepId = id.replace("checkBox","");
+		currentStepId = stepId;
+		const list = totalList.find(list => list.id == currentListId);
+		list.task.forEach(function(task) {
+			if(task.id == currentTaskId) {
+				task.step.forEach(function(newStep) {
+					if(newStep.id == currentStepId) {
+						if (element.checked) {
+							newStep.isFinished = "true";
+						} else {
+							newStep.isFinished = "false";
+						}
+						refreshSteps();
+						addStep(task.step);
+					}
+				});
+			}
+		});
+	});
+}
+
+function listenerForCloseIconInAddStep(element) {
+	element.addEventListener("click", function(event) {
+		console.log("in close");
+		var id = event.target.id;
+		var stepId = id.replace("close","");
+		currentStepId = stepId;
+		const list = totalList.find(list => list.id == currentListId);
+		list.task.forEach(function(task) {
+			if(task.id == currentTaskId) {
+				task.step.forEach(function(newStep) {
+					if(newStep.id == currentStepId) {
+						newStep.status = "inActive";
+						refreshSteps();
+						addStep(task.step);
+					}
+				});
+			}
+		});
+	});
+}
+
+document.querySelector("#checkboxInStepHeader").addEventListener("change", function() {
+	const list = totalList.find(list => list.id == currentListId);
+	list.task.forEach(function(task) {
+		if(task.id == currentTaskId) {
+		    var taskInStepHeader = document.querySelector(".taskInStepHeader");
+			if (task.isFinished == "true") {
+				task.isFinished = "false";
+				taskInStepHeader.setAttribute("class", "taskInStepHeader")
+				refreshTasks();
+				addTask(list);
+			} else {
+				task.isFinished = "true";
+				taskInStepHeader.setAttribute("class", "taskInStepHeader taskInStepHeaderStriked")
+				refreshTasks();
+				addTask(list);
+			}
+		}
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var backButton = document.querySelector(".backIcon");
 backButton.addEventListener("click",function(e) {
 	var sideBarButton = document.querySelector(".sideBarButton");
@@ -335,6 +563,22 @@ backButton.addEventListener("click",function(e) {
 	}
 });
 
-document.querySelector(".deleteList").addEventListener("click",function(e) {
-    
+document.querySelector(".trashIcon").addEventListener("click",function() {
+	console.log("trash icon");
+	const list = totalList.find(list => list.id == currentListId);
+	const task = list.task.find(task => task.id == currentTaskId);
+	let statusDelete = "inActive"
+	task.status = statusDelete;
+	refreshTasks();
+	document.querySelector(".titleName").value = list.name;
+	displayListTasks(list);
 });
+
+function getDate() {
+	date = new Date();
+	millisec = date.getTime();
+	return millisec;
+}
+
+
+
